5bb0983acca26b287675864bd5b44a8857c38a24
Zach Hirst <zach_hirst@live.com>
2019-01-03 13:04:50 -0600

    update

--- memeClicker/memeClicker/Form1.Designer.cs
+++ memeClicker/memeClicker/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::memeClicker.Properties.Resources.red_button_for_web;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me ;)";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblNumber.Location = new System.Drawing.Point(12, 141);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(750, 39);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(774, 647);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblNumber;
     }
 }
 
